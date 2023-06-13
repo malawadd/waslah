e8c515b30ccf7fab65cc3ff959f1b8fc8066ceb1
0xgoldenlion <50057882+malawadd@users.noreply.github.com>
2023-06-13 04:25:43 +0300

    🔥 added sidebar

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,22 +1,50 @@
 import {
   Box,
   Button,
+  CloseButton,
+  Drawer,
+  DrawerContent,
   Flex,
   HStack,
   Icon,
+  Link,
   useColorMode,
+  useDisclosure,
   useColorModeValue,
+  IconButton,
 } from '@chakra-ui/react'
+
+import { FiGitCommit, FiGitMerge, FiMenu } from 'react-icons/fi'
+import { useRouter } from 'next/router'
 import { IoMdMoon, IoMdSunny } from 'react-icons/io'
+
 export default function Layout({ children }) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  
   return (
     <Flex
       flexDirection="column"
       minH="100vh"
       bg={useColorModeValue('gray.100', 'gray.900')}
     >
+      <SidebarContent onClose={() => onClose} display="none" />
+      <Drawer
+        autoFocus={false}
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        returnFocusOnClose={false}
+        onOverlayClick={onClose}
+        size="full"
+      >
+        <DrawerContent>
+          <SidebarContent onClose={onClose} onClick={onClose} />
+        </DrawerContent>
+      </Drawer>
       
-      <MobileNav/>
+      <MobileNav
+       onOpen={onOpen}
+      />
       <Box flex="1" ml={{ base: 0 }} p="4">
         {children}
       </Box>
@@ -27,9 +55,7 @@ export default function Layout({ children }) {
   )
 }
 
-
-
-const MobileNav = ({  ...rest }) => {
+const MobileNav = ({ onOpen, ...rest }) => {
   const navbarIcon = useColorModeValue('brand.500', 'white')
   const { colorMode, toggleColorMode } = useColorMode()
   
@@ -46,7 +72,15 @@ const MobileNav = ({  ...rest }) => {
       justifyContent={{ base: 'space-between' }}
       {...rest}
     >
-     
+     <Flex gap={{ base: 2, md: 4 }} align="center" mr="4" h="100%">
+        <IconButton
+          display={{ base: 'flex' }}
+          onClick={onOpen}
+          variant="outline"
+          aria-label="open menu"
+          icon={<FiMenu />}
+        />
+        </Flex>
 
       <HStack spacing={{ base: '2', md: '6' }}>
        
@@ -71,5 +105,72 @@ const MobileNav = ({  ...rest }) => {
         
       </HStack>
     </Flex>
+  )
+}
+
+const SidebarContent = ({ onClose, ...rest }) => {
+  return (
+    <Box
+      transition="3s ease"
+      bg={useColorModeValue('white', 'gray.900')}
+      borderRight="1px"
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full' }}
+      pos="fixed"
+      h="full"
+      {...rest}
+    >
+      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
+        <Flex display={{ base: 'flex' }} h="100%" align="center">
+        </Flex>
+        <CloseButton onClick={onClose} />
+      </Flex>
+      <NavItem icon={FiGitCommit} hrefPath="/migrate">
+        migrate From mongodb
+      </NavItem>
+      <NavItem icon={FiGitMerge} hrefPath={'/import'}>
+        import inpt  MongoDB
+      </NavItem>
+      
+    </Box>
+  )
+}
+
+const NavItem = ({ icon, hrefPath = '/', children, ...rest }) => {
+  const router = useRouter()
+
+  return (
+    <Link
+      onClick={() => router.push(`${hrefPath}`)}
+      style={{ textDecoration: 'none' }}
+      _focus={{ boxShadow: 'none' }}
+      cursor={'pointer'}
+    >
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: 'brand.400',
+          color: 'white',
+        }}
+        {...rest}
+      >
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{
+              color: 'white',
+            }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </Link>
   )
 }
---
+++ src/pages/migrate/index.tsx
@@ -0,0 +1,112 @@
+import { Step, Steps, useSteps } from 'chakra-ui-steps'
+
+import { Box } from '@chakra-ui/react'
+import Layout from 'layouts/Layout'
+
+import Head from 'next/head'
+import { useState } from 'react'
+import axios from 'axios'
+
+import MongoStepConfigureDb from 'components/mongo/mongo-step-configure-db'
+import MongoStepSelectDb from 'components/mongo/mongo-step-select-db'
+import MongoStepSelectCollections from 'components/mongo/mongo-step-select-collections'
+import MongoStepProceedToImport from 'components/mongo/mongo-step-proceed-to-import'
+import { useDatabaseMigrationStore } from 'contexts/useDatabaseMigrationStore'
+import { NextPageWithLayout } from 'types/Layout'
+
+const MongoWizard: NextPageWithLayout = () => {
+  const { nextStep, prevStep, setStep, reset, activeStep } = useSteps({
+    initialStep: 0,
+  })
+
+  const [databases, setDatabases] = useState([])
+  const [collections, setCollections] = useState([])
+  const [currentDb, setCurrentDb] = useState([])
+  const host = useDatabaseMigrationStore(({ mongoHost }) => mongoHost)
+
+  const getDbs = async () => {
+    let resp = await axios.post(`/api/mongodb/details/get-dbs`, { host })
+    setDatabases(resp.data)
+  }
+
+  const getCollections = async () => {
+    let resp = await axios.post(`/api/mongodb/details/get-collections`, {
+      host,
+      db: currentDb,
+    })
+    setCollections(resp.data)
+  }
+
+  const onClickNextToStep2 = async () => {
+    await getDbs()
+    nextStep()
+  }
+
+  const onClickNextToStep3 = async () => {
+    const collections = await getCollections()
+    console.log(collections)
+    nextStep()
+  }
+
+  const steps = [
+    {
+      label: 'Add Mongo DB Host',
+      content: (
+        <MongoStepConfigureDb handleNextStepClick={onClickNextToStep2} />
+      ),
+    },
+    {
+      label: 'Select Database',
+      content: (
+        <MongoStepSelectDb
+          databases={databases}
+          currentDb={currentDb}
+          setCurrentDb={setCurrentDb}
+          handlePreviousStepClick={prevStep}
+          handleNextStepClick={onClickNextToStep3}
+        />
+      ),
+    },
+    {
+      label: 'Select Collections',
+      content: (
+        <MongoStepSelectCollections
+          collections={collections}
+          handlePreviousStepClick={prevStep}
+          handleNextStepClick={nextStep}
+        />
+      ),
+    },
+    {
+      label: 'Start Migrating',
+      content: (
+        <MongoStepProceedToImport
+          handlePreviousStepClick={prevStep}
+          currentDb={currentDb}
+        />
+      ),
+    },
+  ]
+
+  return (
+    <Box>
+      <Head>
+        <title>Waslah | MongoDB Migration Wizard</title>
+      </Head>
+
+      <Steps activeStep={activeStep} colorScheme={'mongo'}>
+        {steps.map(({ label, content }) => (
+          <Step label={label} key={label}>
+            <Box style={{ padding: 40 }}>{content}</Box>
+          </Step>
+        ))}
+      </Steps>
+    </Box>
+  )
+}
+
+MongoWizard.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>
+}
+
+export default MongoWizard
